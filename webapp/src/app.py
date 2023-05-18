0f7a96fae46c6bcb6d3bd93bccd9225da6926345
Lee <chieh.lee@rakuten.com>
2023-05-19 03:36:44 +0900

    Submission by Chieh Lee

--- webapp/src/app.py
+++ webapp/src/app.py
@@ -1,8 +1,17 @@
+import os
+
 import requests
 from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
+# read config parameters
+# with open('src/config.json', 'r') as c:
+#     config = json.loads(c.read())
+
+# CATEGORY = config["Category"]
+
+CATEGORY = os.getenv("dataCategory", "planets")
 
 @app.get("/")
 def hello():
@@ -10,11 +19,11 @@ def hello():
 
 
 @app.get("/data")
-def get_star_wars_data():
+def get_star_wars_data(id=1):
     try:
         # TODO: Replace the "1" in the URL below with the value of a query parameter
         # TODO: Replace the "people" in the URL below with the value of a configuration parameter
-        response = requests.get("https://swapi.dev/api/people/1", timeout=5)
+        response = requests.get(f"https://swapi.dev/api/{CATEGORY}/{id}", timeout=5)
         response.raise_for_status()
         return response.json()
     except requests.exceptions.HTTPError as e:
@@ -26,6 +35,38 @@ def get_star_wars_data():
 
 
 # TODO: Add new endpoint to return the top 20 people in the Star Wars API with the highest BMI.
+@app.get("/top-people-by-bmi")
+def top_people_by_bmi():
+    try:
+        url = "https://swapi.dev/api/people"
+        top_twenty = []
+        while url:
+            response = requests.get(url, timeout=5)
+            response.raise_for_status()
+
+            for people in response.json()["results"]:
+                if people["mass"] != "unknown" and people["height"] != "unknown":
+                    mass_num = float(people["mass"].replace(",", ""))  # in kg
+                    height_num = float(people["height"].replace(",", ""))  # in centermeter
+                    # add new key for sorting
+                    people["bmi"] = mass_num / ((height_num / 100) ** 2)
+                    top_twenty.append(people)
+
+            url = response.json()["next"]
+        
+        # sort by bmi, get the last 20 and revser the list
+        top_twenty = sorted(top_twenty, key=lambda x: int(x['bmi']))[-20:][::-1]  # slice before reverse
+        # remove key "bmi" as it's not the data provided originally
+        top_twenty = [{category: value for category, value in dude.items() if category != 'bmi'} for dude in top_twenty]
+
+        return top_twenty
+    
+    except requests.exceptions.HTTPError as e:
+        raise HTTPException(status_code=500, detail="API Error")
+    except requests.exceptions.RequestException as e:
+        raise HTTPException(status_code=503, detail="Service Unavailable")
+    except (KeyError, ValueError) as e:
+        raise HTTPException(status_code=500, detail="Data Processing Error")
 
 
 if __name__ == "__main__":
--- webapp/tests/test_get_star_wars_data.py
+++ webapp/tests/test_get_star_wars_data.py
@@ -1,6 +1,15 @@
-from fastapi.testclient import TestClient
+from unittest.mock import patch
 
+import pytest
+from fastapi.testclient import TestClient
+from fastapi import HTTPException
 from webapp.src.app import app
+
+client = TestClient(app)
+
+# To mock exception when service is not available
+def mocked_requests_get(*args, **kwargs):
+    raise HTTPException(status_code=503, detail="Service Unavailable")
 
 def test_get_star_wars_data():
     client = TestClient(app)
@@ -9,3 +18,46 @@ def test_get_star_wars_data():
     assert "name" in response.json()
 
 # Additional test cases can be added by the candidate
+
+def test_get_star_wars_data_bad_input():
+    # Test error case: invalid ID
+    response = client.get("/data?id=invalid")
+    assert response.status_code == 500
+
+    # Test edge case: large ID
+    response = client.get("/data?id=10000")
+    assert response.status_code == 500
+
+
+@patch('fastapi.testclient.TestClient.get', new=mocked_requests_get)
+def test_get_star_wars_data_service_not_available():
+    # Test error case: Service not available
+    with pytest.raises(HTTPException):
+        response = client.get("/data?id=1")
+        assert response.status_code == 503
+        assert response.json()['detail'] == "Service Unavailable"
+
+
+def test_top_people_by_bmi():
+    # Test normal case
+    response = client.get("/top-people-by-bmi")
+    results = response.json()
+
+    # Test response code: Normal
+    assert response.status_code == 200
+
+    # Test that the result contains no more than 20 people
+    assert len(results) <= 20
+
+    # Test that the 'bmi' key is not present in the results
+    for person in results:
+        assert 'bmi' not in person
+
+
+def test_non_existed_endpoint():
+    # Test normal case
+    response = client.get("/i-dont-know-who-i-am")
+    results = response.json()
+
+    # Test response code: Normal
+    assert response.status_code == 404
